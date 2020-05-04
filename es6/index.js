d2570d84e49a83a687c6b1f3aa478587eb4ace1c
James Smith <james.smith@openmathematics.org>
2020-05-04 15:47:45 +0100

    Updated the package.json and index.

---
+++ es6/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+export { default as CustomGrammar } from "./customGrammar";
+export { default as lexersUtilities } from "./utilities/lexers";
+export { default as parsersUtilities } from "./utilities/parsers";
+export { default as CombinedCustomGrammar } from "./combinedCustomGrammar";
--- index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-  CustomGrammar: require('./lib/customGrammar'),
-  lexersUtilities: require('./lib/utilities/lexers'),
-  parsersUtilities: require('./lib/utilities/parsers'),
-  CombinedCustomGrammar: require('./lib/combinedCustomGrammar')
-};
