bd4ba44f2d3aac4391a9674abf13d14284be801b
James Smith <james.smith@openmathematics.org>
2020-06-28 12:24:54 +0100

    Tidied up the fromDirectoryPath() static method a little.

--- es6/customGrammar.js
+++ es6/customGrammar.js
@@ -128,29 +128,63 @@ export default class CustomGrammar {
   }
 
   static fromDirectoryPath(directoryPath) {
-    const lexicalPatternFilePath = `${directoryPath}/pattern.lex`,
-          termBNFFilePath = `${directoryPath}/term.bnf`,
-          expressionBNFFilePath = `${directoryPath}/expression.bnf`,
-          statementBNFFilePath = `${directoryPath}/statement.bnf`,
-          metastatementBNFFilePath = `${directoryPath}/metastatement.bnf`,
-          lexicalPatternFileExists = checkFileExists(lexicalPatternFilePath),
-          termBNFFileExists = checkFileExists(termBNFFilePath),
-          expressionBNFFileExists = checkFileExists(expressionBNFFilePath),
-          statementBNFFileExists = checkFileExists(statementBNFFilePath),
-          metastatementBNFFileExists = checkFileExists(metastatementBNFFilePath),
-          lexicalPatternContent = lexicalPatternFileExists ? readFile(lexicalPatternFilePath) : null,
-          termBNFContent = termBNFFileExists ? readFile(termBNFFilePath) : null,
-          expressionBNFContent = expressionBNFFileExists ? readFile(expressionBNFFilePath) : null,
-          statementBNFContent = statementBNFFileExists ? readFile(statementBNFFilePath) : null,
-          metastatementBNFContent = metastatementBNFFileExists ? readFile(metastatementBNFFilePath) : null,
-          name = directoryPath, ///
-          lexicalPattern = lexicalPatternContent, ///
-          termBNF = termBNFContent, ///
-          expressionBNF = expressionBNFContent, ///
-          statementBNF = statementBNFContent, ///
-          metastatementBNF = metastatementBNFContent, ///
+    const name = directoryPath, ///
+          lexicalPattern = lexicalPatternFromDirectoryPath(directoryPath),
+          termBNF = termBNFFromDirectoryPath(directoryPath),
+          expressionBNF = expressionBNFFromDirectoryPath(directoryPath),
+          statementBNF = statementBNFFromDirectoryPath(directoryPath),
+          metastatementBNF = metastatementBNFFromDirectoryPath(directoryPath),
           customGrammar = new CustomGrammar(name, lexicalPattern, termBNF, expressionBNF, statementBNF, metastatementBNF);
 
     return customGrammar;
   }
+}
+
+function lexicalPatternFromDirectoryPath(directoryPath) {
+  const lexicalPatternFilePath = `${directoryPath}/pattern.lex`,
+        lexicalPatternFileExists = checkFileExists(lexicalPatternFilePath),
+        lexicalPatternContent = lexicalPatternFileExists ?
+                                  readFile(lexicalPatternFilePath) :
+                                    null,
+        lexicalPattern = lexicalPatternContent; ///
+
+  return lexicalPattern;
+}
+
+function metastatementBNFFromDirectoryPath(directoryPath) {
+  const metastatementBNFFilePath = `${directoryPath}/metastatement.bnf`,
+        metastatementBNF = bnfFromBNFFilePath(metastatementBNFFilePath);
+
+  return metastatementBNF;
+}
+
+function statementBNFFromDirectoryPath(directoryPath) {
+  const statementBNFFilePath = `${directoryPath}/statement.bnf`,
+        statementBNF = bnfFromBNFFilePath(statementBNFFilePath);
+
+  return statementBNF;
+}
+
+function expressionBNFFromDirectoryPath(directoryPath) {
+  const expressionBNFFilePath = `${directoryPath}/expression.bnf`,
+        expressionBNF = bnfFromBNFFilePath(expressionBNFFilePath);
+
+  return expressionBNF;
+}
+
+function termBNFFromDirectoryPath(directoryPath) {
+  const termBNFFilePath = `${directoryPath}/term.bnf`,
+      termBNF = bnfFromBNFFilePath(termBNFFilePath);
+
+  return termBNF;
+}
+
+function bnfFromBNFFilePath(bnfFilePath) {
+  const bnfFileExists = checkFileExists(bnfFilePath),
+        bnfContent = bnfFileExists ?
+                       readFile(bnfFilePath) :
+                         null,
+        bnf = bnfContent; ///
+
+  return bnf;
 }
