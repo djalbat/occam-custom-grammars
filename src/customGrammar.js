5652d8a59201d72b331ebeb4be7c93a76892ff40
James Smith <james.smith@djalbat.com>
2021-03-24 18:53:09 +0000

    Migrated to SWC and ESBuild.

--- src/customGrammar.js
+++ src/customGrammar.js
@@ -1,9 +1,5 @@
 "use strict";
 
-import { fileSystemUtilities } from "necessary";
-
-const { readFile, checkFileExists } = fileSystemUtilities;
-
 export default class CustomGrammar {
   constructor(name, lexicalPattern, termBNF, expressionBNF, statementBNF, metastatementBNF) {
     this.name = name;
@@ -126,65 +122,4 @@ export default class CustomGrammar {
 
     return customGrammar;
   }
-
-  static fromDirectoryPath(directoryPath) {
-    const name = directoryPath, ///
-          lexicalPattern = lexicalPatternFromDirectoryPath(directoryPath),
-          termBNF = termBNFFromDirectoryPath(directoryPath),
-          expressionBNF = expressionBNFFromDirectoryPath(directoryPath),
-          statementBNF = statementBNFFromDirectoryPath(directoryPath),
-          metastatementBNF = metastatementBNFFromDirectoryPath(directoryPath),
-          customGrammar = new CustomGrammar(name, lexicalPattern, termBNF, expressionBNF, statementBNF, metastatementBNF);
-
-    return customGrammar;
-  }
-}
-
-function lexicalPatternFromDirectoryPath(directoryPath) {
-  const lexicalPatternFilePath = `${directoryPath}/pattern.lex`,
-        lexicalPatternFileExists = checkFileExists(lexicalPatternFilePath),
-        lexicalPatternContent = lexicalPatternFileExists ?
-                                  readFile(lexicalPatternFilePath) :
-                                    null,
-        lexicalPattern = lexicalPatternContent; ///
-
-  return lexicalPattern;
-}
-
-function metastatementBNFFromDirectoryPath(directoryPath) {
-  const metastatementBNFFilePath = `${directoryPath}/metastatement.bnf`,
-        metastatementBNF = bnfFromBNFFilePath(metastatementBNFFilePath);
-
-  return metastatementBNF;
-}
-
-function statementBNFFromDirectoryPath(directoryPath) {
-  const statementBNFFilePath = `${directoryPath}/statement.bnf`,
-        statementBNF = bnfFromBNFFilePath(statementBNFFilePath);
-
-  return statementBNF;
-}
-
-function expressionBNFFromDirectoryPath(directoryPath) {
-  const expressionBNFFilePath = `${directoryPath}/expression.bnf`,
-        expressionBNF = bnfFromBNFFilePath(expressionBNFFilePath);
-
-  return expressionBNF;
-}
-
-function termBNFFromDirectoryPath(directoryPath) {
-  const termBNFFilePath = `${directoryPath}/term.bnf`,
-      termBNF = bnfFromBNFFilePath(termBNFFilePath);
-
-  return termBNF;
-}
-
-function bnfFromBNFFilePath(bnfFilePath) {
-  const bnfFileExists = checkFileExists(bnfFilePath),
-        bnfContent = bnfFileExists ?
-                       readFile(bnfFilePath) :
-                         null,
-        bnf = bnfContent; ///
-
-  return bnf;
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 export { DEFAULT_CUSTOM_GRAMMAR_NAME } from "./constants";
+
 export { default as CustomGrammar } from "./customGrammar";
 export { default as lexersUtilities } from "./utilities/lexers";
 export { default as parsersUtilities } from "./utilities/parsers";
