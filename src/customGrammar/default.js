fc36c14bc6b3f6fc45070613f3b3f0a71de57831
James Smith <james.smith@djalbat.com>
2024-08-29 10:09:55 +0100

    Propagated.

--- src/customGrammar/default.js
+++ src/customGrammar/default.js
@@ -44,8 +44,6 @@ export const metastatementBNF = `metastatement                        ::=  "(" m
 
                                        |  metavariable substitution?
 
-                                       |  statement
-       
                                        ;
                                        
 subproofAssertion                    ::=  "[" metastatement ( "," metastatement )* "]" "..." metastatement ;
