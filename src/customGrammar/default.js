ae979ba64704ffeb0a92f959ce6c1d183ccd9a2a
James Smith <james.smith@djalbat.com>
2024-09-05 14:58:40 +0100

    Changed the substitution BNF.

--- src/customGrammar/default.js
+++ src/customGrammar/default.js
@@ -50,7 +50,7 @@ frame                                ::=  ( declaration | ( "..."<NO_WHITESPACE>
  
 declaration                          ::=  reference "::" statement ;
 
-substitution                         ::=  <NO_WHITESPACE>"[" term "for" term "]" ;`;
+substitution                         ::=  <NO_WHITESPACE>"[" term "for" variable "]" ;`;
 
 const name = DEFAULT_CUSTOM_GRAMMAR_NAME,
       json = {
