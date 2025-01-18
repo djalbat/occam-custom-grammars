f77d64366047e8ab8a9a35c5dcfc3cf797d4890e
James Smith <james.smith@djalbat.com>
2025-01-18 13:42:31 +0000

    Changed the leftmost term to a variable in property assertions.

--- src/customGrammar/default.js
+++ src/customGrammar/default.js
@@ -48,7 +48,7 @@ definedAssertion                     ::=  ( frame | term ) "is" ( "defined" | "u
 
 containedAssertion                   ::=  ( frame | term ) "is" ( "present" | "missing" ) "in" statement ;
 
-propertyAssertion                    ::=  term "is" ( "a" | "an" ) property "of" term ;
+propertyAssertion                    ::=  variable "is" ( "a" | "an" ) property "of" term ;
 
 subproofAssertion                    ::=  "[" statement ( "," statement )* "]" "..." statement ;
 
