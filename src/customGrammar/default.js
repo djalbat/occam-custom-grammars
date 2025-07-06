71e0ee02f0c614a8d8b88c5befd3a08def9ef402
James Smith <james.smith@djalbat.com>
2025-07-06 17:38:06 +0100

    Changed the base type back to the object type.

--- src/customGrammar/default.js
+++ src/customGrammar/default.js
@@ -62,7 +62,7 @@ statementSubstitution                ::=  "[" statement "for" statement "]";
 
 referenceSubstitution                ::=  "[" reference "for" reference "]";`;
 
-export const typePattern = "Base";
+export const typePattern = "Object";
 
 export const symbolPattern = "::|\\[|\\]|\\.\\.\\.|\\|-|=|defined|undefined|missing|present";
 
