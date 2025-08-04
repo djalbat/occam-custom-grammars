9ae2339ef377498799ba6a28afcedc8a529ea11a
James Smith <james.smith@djalbat.com>
2025-08-04 20:29:43 +0100

    Changed terms to arguments in the satisfying assertion rule.

--- src/customGrammar/default.js
+++ src/customGrammar/default.js
@@ -42,7 +42,7 @@ definedAssertion                     ::=  ( frame | term ) "is" ( "defined" | "u
 
 containedAssertion                   ::=  ( frame | term ) "is" ( "present" | "missing" ) "in" statement ;
 
-satisfiesAssertion                   ::=  ( ( term "satisfies" ) | ( term ( "," term )+ "satisfy" ) ) metavariable ;  
+satisfiesAssertion                   ::=  ( ( argument "satisfies" ) | ( argument ( "," argument )+ "satisfy" ) ) metavariable ;  
 
 subproofAssertion                    ::=  "[" statement ( "," statement )* "]" "..." statement ;
 
