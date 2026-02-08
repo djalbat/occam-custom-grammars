101b8c593cfe7ad49e3a3b73c3dbc3017a61a583
James Smith <james.smith@djalbat.com>
2026-02-08 09:04:07 +0000

    Added the custom grammar utilities.

---
+++ src/utilities/customGrammar.js
@@ -0,0 +1,45 @@
+"use strict";
+
+import CustomGrammar from "../customGrammar";
+import CombinedCustomGrammar from "../customGrammar/combined";
+
+export function combinedCustomGrammarFromJSON(json) {
+  const customGrammarsJSON = json,  ///
+        customGrammars = customGrammarsJSON.mao((customGrammarJSON) => {
+          const json = customGrammarJSON, ///
+                customGrammar = CustomGrammar.fromJSON(json);
+
+          return customGrammar;
+        });
+
+  customGrammars.reverse(); //
+
+  const combinedCustomGrammar = CombinedCustomGrammar.fromCustomGrammars(customGrammars);
+
+  return combinedCustomGrammar;
+}
+
+export function combinedCustomGrammarFromNothing() {
+  const customGrammars = [],
+        combinedCustomGrammar = CombinedCustomGrammar.fromCustomGrammars(customGrammars);
+
+  return combinedCustomGrammar;
+}
+
+export function combinedCustomGrammarFromCustomGrammars(customGrammars) {
+  customGrammars = [  ///
+    ...customGrammars
+  ];
+
+  customGrammars.reverse(); ///
+
+  const combinedCustomGrammar = CombinedCustomGrammar.fromCustomGrammars(customGrammars);
+
+  return combinedCustomGrammar;
+}
+
+export default {
+  combinedCustomGrammarFromJSON,
+  combinedCustomGrammarFromNothing,
+  combinedCustomGrammarFromCustomGrammars
+};
